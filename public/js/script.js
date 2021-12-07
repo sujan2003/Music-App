296c1b418d6ee7c28d797fd6d1081c51d3fcdc96
elaineliuwang <elaine.liuwang@gmail.com>
2021-12-06 20:49:48 -0600

    apl stuff friday changes

--- public/js/script.js
+++ public/js/script.js
@@ -2,44 +2,86 @@ const musicContainer = document.getElementById("music-container")
 const playBtn = document.getElementById("play")
 const prevBtn = document.getElementById("prev")
 const nextBtn = document.getElementById("next")
-// const audio = document.getElementById("audio")
-const sunny1 = new Audio("/music/Sunday Morning - Maroon 5.mp3")
-const sunny2 = new Audio("/music/Watermelon Sugar - Harry Styles.mp3")
-const sunny3 = new Audio("/music/Peaches - Justin Bieber.mp3")
-const cloudy1 = new Audio("/music/Rolling in the Deep - Adele.mp3")
-const cloudy2 = new Audio("/music/Fly Me to the Moon - Frank Sinatra.mp3")
-const cloudy3 = new Audio("/music/Night Changes - 1D.mp3")
-const rain1 = new Audio("/music/Perfect - Ed Sheeran.mp3")
-const rain2 = new Audio("/music/Can't Help Falling in Love - Elvis Presley.mp3")
-const rain3 = new Audio("/music/Right Here Waiting - Richard Marx.mp3")
-const thunderstorm1 = new Audio("/music/Before You Go - Lewis Capaldi.mp3")
-const thunderstorm2 = new Audio("/music/Train Wreck - James Arthur.mp3")
-const thunderstorm3 = new Audio("/music/Lightning - Little Mix.mp3")
-const snow1 = new Audio("/music/cardigan - Taylor Swift.mp3")
-const snow2 = new Audio("/music/Mystery of Love - Sufjan Stevens.mp3")
-const snow3 = new Audio("/music/Electric Feel - CRW.mp3")
+
+const audio = document.getElementById("sm-m5")
+const cover = document.getElementById("sm-m5-cvr")
+const title = document.getElementById("title")
+const artist = document.getElementById("artist")
+
+// Audio Objects 
+// i literally could find no way to use audio objects in a music player, i scoured the internet far and wide and still nothing
+// so for now, i just used an html audio and coverimg and it works fine
+/*const sunny1 = new Audio("/music/sm-m5.mp3")
+const sunny2 = new Audio("/music/ws-hs.mp3")
+const sunny3 = new Audio("/music/p-jb.mp3")
+const cloudy1 = new Audio("/music/ritd-a.mp3")
+const cloudy2 = new Audio("/music/fmttm-fs.mp3")
+const cloudy3 = new Audio("/music/nc-od.mp3")
+const rain1 = new Audio("/music/p-es.mp3")
+const rain2 = new Audio("/music/chfil-ep.mp3")
+const rain3 = new Audio("/music/rhw-rm.mp3")
+const thunderstorm1 = new Audio("/music/byg-lc.mp3")
+const thunderstorm2 = new Audio("/music/tw-ja.mp3")
+const thunderstorm3 = new Audio("/music/l-lm.mp3")
+const snow1 = new Audio("/music/c-ts.mp3")
+const snow2 = new Audio("/music/mol-ss.mp3")
+const snow3 = new Audio("/music/ef-crw.mp3")*/
+
 const progress = document.getElementById("progress")
-const title = document.getElementById('title')
 const progressContainer = document.getElementById("progress-container")
 
-import {Song} from "./modules/Song"
 
-console.log(Song)
-// const cover = document.getElementById("cover")
+//commented out so the music player works
+    // import {Song} from "./modules/Song"
+    // console.log(Song)
 
 // song playlists
 const sunny = [
-    "Sunday Morning - Maroon 5", 
-    "Watermelon Sugar - Harry Styles", 
-    "Peaches - Justin Bieber"
+    {
+        title: "Sunday Morning",
+        artist: "Maroon 5",
+        img: "sm-m5-cvr",
+        music: "sm-m5"
+    }, 
+
+    {
+        title: "Watermelon Sugar", 
+        artist: "Harry Styles",
+        img: "ws-hs-cvr",
+        music: "ws-hs"
+    }, 
+
+    {
+        title: "Peaches",
+        artist: "Justin Bieber",
+        img: "p-jb-cvr",
+        music: "p-jb"
+    }
 ]
 
 const cloudy = [
-    "Rolling in the Deep - Adele",
-    "Fly Me to the Moon - Frank Sinatra", 
-    "Night Changes - 1D"
-]
+    {
+        title: "Rolling in the Deep",
+        artist: "Adele",
+        img: "ritd-a-cvr",
+        music: "ritd-a"
+    },
 
+    {
+        title: "Fly Me to the Moon", 
+        artist: "Frank Sinatra",
+        img: "fmttm-fs-cvr",
+        music: "fmttm-fs"
+    },
+
+    {
+        title: "Night Changes",
+        artist: "One Direction",
+        img: "nc-od-cvr",
+        music: "nc-od"
+    }
+]
+// these next 3 playlists will need to have title, artist, img, music added to it like the ones above
 const rain = [
     "Perfect - Ed Sheeran", 
     "Can't Help Falling in Love - Elvis Presley", 
@@ -58,6 +100,8 @@ const snow = [
     "Electric Feel - CRW"
 ]
 
+const playlists = ["sunny", "cloudy", "rain", "thunderstorm", "snow"]; //use this to determine which playlist to play? idk
+
 // const songs = new Audio("/music/APLmusic.mp3");
 
 
@@ -65,13 +109,14 @@ const snow = [
 let songIndex = 0
 
 // load song info into dom
-loadSong(sunny[songIndex])
+loadSong(sunny[songIndex]) // currently the loadSong functions with the assigned playlist, in this case i assigned it 'sunny' for testing purposes
 
 // updates details of the song
 function loadSong(song){
-    title.textContent = song
-    audio.src = `music/${song}.mp3`
-    cover.src = `images/${song}.jpeg` 
+    title.innerText = song.title;
+    artist.innerText = song.artist;
+    audio.src = `/music/${song.music}.mp3`;
+    cover.src = `/images/${song.img}.jpeg`;
 }
 
 function playSong(){
@@ -94,21 +139,21 @@ function prevSong() {
     songIndex--;
 
     if(songIndex < 0){
-        songIndex = songs.length - 1
+        songIndex = sunny.length - 1 //only works with 'sunny' playlist right now
     }
 
-    loadSong(songs[songIndex])
+    loadSong(sunny[songIndex]) //only works with 'sunny' playlist right now
     playSong()
 }
 
 function nextSong() {
     songIndex++;
 
-    if(songIndex > songs.length-1){
+    if(songIndex > sunny.length-1){ //only works with 'sunny' playlist right now
         songIndex = 0
     }
 
-    loadSong(songs[songIndex])
+    loadSong(sunny[songIndex]) //only works with 'sunny' playlist right now
     playSong()
 }
 
@@ -116,7 +161,6 @@ function updateProgress(x){
     const {duration, currentTime} = x.srcElement
     const progressPercent = (currentTime/duration)*100
     progress.style.width = `${progressPercent}%`
-    console.log('yoyoyo')
 }
 
 function setProgress(x){
