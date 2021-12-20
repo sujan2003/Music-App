8c7b26df2cdb995adfb01dc5796f83ec10aa9e46
sean <sean.mcbroom@outlook.com>
2021-12-20 13:06:45 -0600

    Add documentation

--- src/public/scripts/modules/Events.js
+++ src/public/scripts/modules/Events.js
@@ -1,8 +1,16 @@
 class Events {
+    /**
+     * Create new events object
+     */
     constructor() {
         this.triggers = {};
     }
 
+    /**
+     * Runs callback on event trigger
+     * @param {String} event Name of event
+     * @param {Function} callback Callback function to run
+     */
     on(event, callback) {
         const triggers = this.triggers;
 
@@ -12,6 +20,11 @@ class Events {
         }
     }
 
+    /**
+     * Trigger event callbacks
+     * @param {String} event Name of event
+     * @param {{}} params Parameters to pass to callback functions 
+     */
     trigger(event, params) {
         const triggers = this.triggers;
 
