3d0b6256de2c67675c71de707081b1b02d277a53
sean <sean.mcbroom@outlook.com>
2021-12-21 18:56:08 -0600

    Add once method to events

--- src/public/scripts/modules/Events.js
+++ src/public/scripts/modules/Events.js
@@ -16,7 +16,21 @@ class Events {
 
         if (!triggers[event]) {
             if (!triggers[event]) triggers[event] = []
-            triggers[event].push(callback);
+            triggers[event].push({ callback: callback, type: "on" });
+        }
+    }
+
+    /**
+     * Runs callback on event trigger (only once)
+     * @param {String} event Name of event
+     * @param {Function} callback Callback function to run
+     */
+    once(event, callback) {
+        const triggers = this.triggers;
+
+        if (!triggers[event]) {
+            if (!triggers[event]) triggers[event] = []
+            triggers[event].push({ callback: callback, type: "once" });
         }
     }
 
@@ -30,7 +44,11 @@ class Events {
 
         if (triggers[event]) {
             for (let i in triggers[event]) {
-                triggers[event][i](params);
+                triggers[event][i]["callback"](params);
+
+                if (triggers[event][i]["type"] == "once") {
+                    delete triggers[event][i];
+                }
             }
         }
     }
